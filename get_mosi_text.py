e5178d2183ddaa86bdaeacf95f2f461ea3327785
MR. WANG <hrwang@kfsrv03.se.cuhk.edu.hk>
2020-10-10 22:09:26 +0800

    dataset done

--- get_mosi_text.py
+++ get_mosi_text.py
@@ -1,10 +1,27 @@
 from mmsdk import mmdatasdk as md
 import os
 
-def get_mosi_text(save_name):
-	fout = open(save_name, 'w')
+def get_mosi_train():
+	return md.cmu_mosi.standard_folds.standard_train_fold
+
+
+def get_mosi_val():
+	return md.cmu_mosi.standard_folds.standard_valid_fold
+
+
+def get_mosi_test():
+	return md.cmu_mosi.standard_folds.standard_test_fold
+
+
+def get_mosi_text():
+	train = get_mosi_train()
+	val = get_mosi_val()
+	test = get_mosi_test()
+	ftrain = open('mosi_text_train.txt', 'w')
+	fval = open('mosi_text_val.txt', 'w')
+	ftest = open('mosi_text_test.txt', 'w')
 	dataset = load_dataset()
-    text_files = os.listdir("/misc/kfdata01/kf_grp/hrwang/socialKG/raw_data/Raw/Transcript/Segmented")
+	text_files = os.listdir("/misc/kfdata01/kf_grp/hrwang/socialKG/raw_data/Raw/Transcript/Segmented")
 	for text_file in text_files:
 		video_name = text_file.split('.')[0]
 		with open(os.path.join("/misc/kfdata01/kf_grp/hrwang/socialKG/raw_data/Raw/Transcript/Segmented", text_file), 'r') as f:
@@ -14,9 +31,17 @@ def get_mosi_text(save_name):
 				text = line.replace(str(i) + '_DELIM_', '')
 				label = get_mosi_label(dataset, video_name, i-1)
 				i += 1
-				fout.write(text + ',' + str(label) + '\n')
+				if video_name in train:
+					ftrain.write(text + ',' + str(label) + '\n')
+				if video_name in val:
+					fval.write(text + ',' + str(label) + '\n')
+				if video_name in test:
+					ftest.write(text + ',' + str(label) + '\n')
+					
 
-	fout.close()
+	ftrain.close()
+	fval.close()
+	ftest.close()
 
 
 def load_dataset():
@@ -44,4 +69,4 @@ def get_mosi_label(dataset, video_id, segement_id):
 	return dataset[label_field][video_id]['features'][segement_id][0]
 
 if __name__ == '__main__':
-	get_mosi_text('mosi_text_data.txt')
+	get_mosi_text()
