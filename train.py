c72dd583a409b94da134a9cd62230985c6d53822
ZhiHao Wang <1397665447@qq.com>
2020-10-13 03:13:23 +0800

    v 1.0

--- train.py
+++ train.py
@@ -1,82 +1,74 @@
-# from transformers import BertForSequenceClassification
-# from transformers import AdamW
-# from transformers import BertTokenizer
-
-# model = BertForSequenceClassification.from_pretrained('bert-base-uncased', num_labels=2)
-# model.train()
-# optimizer = AdamW(model.parameters(), lr=1e-5)
-
-# tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-# text_batch = ["I love Pixar.", "I don't care for Pixar."]
-# encoding = tokenizer(text_batch, return_tensors='pt', padding=True, truncation=True)
-# input_ids = encoding['input_ids']
-# attention_mask = encoding['attention_mask']
-
-# labels = torch.tensor([1,0]).unsqueeze(0)
-# outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
-# loss = outputs.loss
-# loss.backward()
-# optimizer.step()
-
-  
-import os
 import torch
-from torch.utils import data
-from PIL import Image
-import cv2
-import json
+import random
+from transformers import BertForSequenceClassification
+from transformers import AdamW
 import numpy as np
-
-class Dataset(data.Dataset):
-    'Characterizes a dataset for PyTorch'
-    def __init__(self, list_path, transform, train=True):
-        'Initialization'
-        fin = open(list_path, 'r')
-        self.meta = fin.readlines()
-        fin.close()
-        print('load meta: ', list_path)
-        self.transform = transform
-        self.train = train
-
-    def __len__(self):
-        'Denotes the total number of samples'
-        return len(self.meta)
-
-    def __getitem__(self, index):
-        'Generates one sample of data'
-        image_path, label = self.meta[index].split()
-        label = int(label)
-        image = Image.open(image_path) 
-        if self.transform is not None:
-            sample = self.transform(image)
-        if self.train:
-            return sample, label
-        else:
-            return image_path, sample, label
+from transformers import BertTokenizer
+from torch.utils.data import TensorDataset, DataLoader, random_split
+from utils import get_data, encode_fn
+from transformers import get_linear_schedule_with_warmup
 
 
-from transformers import BertForSequenceClassification, Trainer, TrainingArguments
+batch_size = 1
+epochs = 50
+seed = 42
+random.seed(seed)
+np.random.seed(seed)
+torch.manual_seed(seed)
+torch.cuda.manual_seed_all(seed)
+torch.backends.cudnn.deterministic = True
 
-#model = BertForSequenceClassification.from_pretrained("bert-large-uncased")
-model = BertForSequenceClassification.from_pretrained('bert-base-uncased', num_labels=)
+device = torch.device('cuda')
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
+train_text, train_labels = get_data('mosi_text_train.txt')
+val_text, val_labels = get_data('mosi_text_val.txt')
 
-training_args = TrainingArguments(
-    output_dir='./results',          # output directory
-    num_train_epochs=3,              # total # of training epochs
-    per_device_train_batch_size=16,  # batch size per device during training
-    per_device_eval_batch_size=64,   # batch size for evaluation
-    warmup_steps=500,                # number of warmup steps for learning rate scheduler
-    weight_decay=0.01,               # strength of weight decay
-    logging_dir='./logs',            # directory for storing logs
-)
+train_input_ids = encode_fn(train_text, tokenizer)
+train_labels = torch.tensor(train_labels)
 
-trainer = Trainer(
-    model=model,                         # the instantiated 🤗 Transformers model to be trained
-    args=training_args,                  # training arguments, defined above
-    train_dataset=train_dataset,         # training dataset
-    eval_dataset=test_dataset            # evaluation dataset
-)
+val_input_ids = encode_fn(val_text, tokenizer)
+val_labels = torch.tensor(val_labels)
 
-trainer.train()
-trainer.evaluate()
-model.save_pretrained(save_directory)
+train_dataset = TensorDataset(train_input_ids, train_labels)
+val_dataset = TensorDataset(val_input_ids, val_labels)
+
+train_dataloader = DataLoader(train_dataset, batch_size = batch_size, shuffle = True)
+val_dataloader = DataLoader(val_dataset, batch_size = batch_size, shuffle = False)
+
+model = BertForSequenceClassification.from_pretrained('bert-base-uncased', num_labels=1, output_attentions=False, output_hidden_states=False)
+model.cuda()
+
+
+optimizer = AdamW(model.parameters(), lr=2e-5)
+total_steps = len(train_dataloader) * epochs
+scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=0, num_training_steps=total_steps)
+
+
+for epoch in range(epochs):
+    model.train()
+    total_loss, total_val_loss = 0, 0
+    for step, batch in enumerate(train_dataloader):
+        model.zero_grad()
+        loss, logits = model(batch[0].to(device), token_type_ids=None, attention_mask=(batch[0]>0).to(device), labels=batch[1].to(device))
+        total_loss += loss.item()
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+        optimizer.step() 
+        scheduler.step()
+        
+    model.eval()
+    for i, batch in enumerate(val_dataloader):
+        with torch.no_grad():
+            loss, logits = model(batch[0].to(device), token_type_ids=None, attention_mask=(batch[0]>0).to(device), labels=batch[1].to(device))
+                
+            total_val_loss += loss.item()
+            
+            logits = logits.detach().cpu().numpy()
+            label_ids = batch[1].to('cpu').numpy()
+    
+    avg_train_loss = total_loss / len(train_dataloader)
+    avg_val_loss = total_val_loss / len(val_dataloader)
+    
+    print('Train loss     : {avg_train_loss}')
+    print('Validation loss: {avg_val_loss}')
+    print('\n')
---
+++ utils.py
@@ -0,0 +1,31 @@
+import os
+import torch
+
+def get_data(file_path):
+    if not os.path.exists(file_path):
+        print('file not exisits.')
+        exit(0)
+    print('loading ', file_path)
+    text = []
+    labels = []
+    with open(file_path, 'r') as f:
+        for line in f.readlines():
+            words, label = line.split(',')
+            text.append(words)
+            labels.append(float(label))
+    return text, labels
+
+
+def encode_fn(text_list, tokenizer):
+    all_input_ids = []    
+    for text in text_list:
+        input_ids = tokenizer.encode(
+                        text,                      
+                        add_special_tokens = True,  
+                        max_length = 80,        
+                        pad_to_max_length = True,   
+                        return_tensors = 'pt'       
+                   )
+        all_input_ids.append(input_ids)    
+    all_input_ids = torch.cat(all_input_ids, dim=0)
+    return all_input_ids
