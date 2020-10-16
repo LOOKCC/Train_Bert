7aa72bfa9a4f51739235550c3cb47af2289dad1f
XiangruTang <xarutang@gmail.com>
2020-10-16 11:50:03 +0000

    cls

--- train.py
+++ train.py
@@ -5,12 +5,12 @@ from transformers import AdamW
 import numpy as np
 from transformers import BertTokenizer
 from torch.utils.data import TensorDataset, DataLoader, random_split
-from utils import get_data, encode_fn
+from utils import get_data, encode_fn ,flat_accuracy
 from transformers import get_linear_schedule_with_warmup
 
 
 batch_size = 4
-epochs = 50
+epochs = 10
 seed = 42
 random.seed(seed)
 np.random.seed(seed)
@@ -35,7 +35,7 @@ val_dataset = TensorDataset(val_input_ids, val_labels)
 train_dataloader = DataLoader(train_dataset, batch_size = batch_size, shuffle = True)
 val_dataloader = DataLoader(val_dataset, batch_size = batch_size, shuffle = False)
 
-model = BertForSequenceClassification.from_pretrained('bert-base-uncased', num_labels=1, output_attentions=False, output_hidden_states=False)
+model = BertForSequenceClassification.from_pretrained('bert-base-uncased', num_labels=2, output_attentions=False, output_hidden_states=False)
 model.cuda()
 
 
@@ -48,6 +48,7 @@ for epoch in range(epochs):
     print('epoch:', epoch)
     model.train()
     total_loss, total_val_loss = 0, 0
+    total_eval_accuracy = 0
     for step, batch in enumerate(train_dataloader):
         model.zero_grad()
         loss, logits = model(batch[0].to(device), token_type_ids=None, attention_mask=(batch[0]>0).to(device), labels=batch[1].to(device))
@@ -66,12 +67,15 @@ for epoch in range(epochs):
             
             logits = logits.detach().cpu().numpy()
             label_ids = batch[1].to('cpu').numpy()
+            total_eval_accuracy += flat_accuracy(logits, label_ids)
     
     avg_train_loss = total_loss / len(train_dataloader)
     avg_val_loss = total_val_loss / len(val_dataloader)
+    avg_val_accuracy = total_eval_accuracy / len(val_dataloader)
     
     print('Train loss', avg_train_loss)
     print('Validation loss', avg_val_loss)
+    print('Accuracy:', avg_val_accuracy)
 
 tokenizer.save_pretrained('tokenizer')
 model.save_pretrained('bert_model')
--- utils.py
+++ utils.py
@@ -1,5 +1,12 @@
 import os
 import torch
+import numpy as np
+from sklearn.metrics import f1_score, accuracy_score
+
+def flat_accuracy(preds, labels):
+    pred_flat = np.argmax(preds, axis=1).flatten()
+    labels_flat = labels.flatten()
+    return accuracy_score(labels_flat, pred_flat)
 
 def get_data(file_path):
     if not os.path.exists(file_path):
@@ -12,7 +19,10 @@ def get_data(file_path):
         for line in f.readlines():
             words, label = line.split(',')
             text.append(words)
-            labels.append(float(label))
+            if float(label) > 0:
+                labels.append(1)
+            if float(label) <= 0:
+                labels.append(0)
     return text, labels
 
 
@@ -28,3 +38,7 @@ def encode_fn(text_list, tokenizer):
         all_input_ids.append(input_ids)    
     all_input_ids = torch.cat(all_input_ids, dim=0)
     return all_input_ids
+
+
+
+
